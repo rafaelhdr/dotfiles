249428d3f7f7cdaaf88ccf7dd74dc6a2627a2886
Rafael Hurpia da Rocha <1108973+rafaelhdr@users.noreply.github.com>
2025-08-16 15:47:50 +0200

    fix: ghostty to symlinks

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -5,5 +5,6 @@ echo "Creating symbolic links..."
 ln -sf ~/dotfiles/config/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/config/.config/nvim/ ~/.config/
 ln -sf ~/dotfiles/config/.config/kitty/ ~/.config/
+ln -sf ~/dotfiles/config/.config/ghostty/ ~/.config/
 
 echo "Symbolic links created successfully."
