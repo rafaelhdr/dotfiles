006d16c8efcb844b9525b0eb6252f6e9351d27fb
Rafael Hurpia da Rocha <git@rafaelhdr.com.br>
2026-03-05 23:29:11 +0100

    nvim: convert tabs to spaces in keymaps.lua and diagnostics.lua

--- config/.config/nvim/lua/config/keymaps.lua
+++ config/.config/nvim/lua/config/keymaps.lua
@@ -1,19 +1,19 @@
 -- stevearc/conform
 vim.keymap.set("n", "<leader>fa", function()
-	require("conform").format({ timeout_ms = 3000, lsp_fallback = false })
+  require("conform").format({ timeout_ms = 3000, lsp_fallback = false })
 end)
 vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 
 -- lsp
 vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
 vim.keymap.set("n", "<leader>gD", function()
-	local params = vim.lsp.util.make_position_params()
-	vim.lsp.buf_request(0, "textDocument/definition", params, function(err, result, ctx)
-		if err or not result or vim.tbl_isempty(result) then
-			return
-		end
+  local params = vim.lsp.util.make_position_params()
+  vim.lsp.buf_request(0, "textDocument/definition", params, function(err, result, ctx)
+    if err or not result or vim.tbl_isempty(result) then
+      return
+    end
 
-		vim.cmd("tabnew")
-		vim.lsp.util.jump_to_location(result[1], "utf-8")
-	end)
+    vim.cmd("tabnew")
+    vim.lsp.util.jump_to_location(result[1], "utf-8")
+  end)
 end, { desc = "Go to definition (new tab)" })
--- config/.config/nvim/lua/plugins/diagnostics.lua
+++ config/.config/nvim/lua/plugins/diagnostics.lua
@@ -1,10 +1,10 @@
 return {
-	"rachartier/tiny-inline-diagnostic.nvim",
-	event = "VeryLazy",
-	priority = 1000,
-	config = function()
-		require("tiny-inline-diagnostic").setup({
+  "rachartier/tiny-inline-diagnostic.nvim",
+  event = "VeryLazy",
+  priority = 1000,
+  config = function()
+    require("tiny-inline-diagnostic").setup({
       preset = "powerline",
     })
-	end,
+  end,
 }
