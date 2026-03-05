8ab81d52eb40432119632cfeeb2838c99df6cd68
Rafael Hurpia da Rocha <git@rafaelhdr.com.br>
2026-03-05 23:25:47 +0100

    nvim: enable ts_ls and move gd keymap to keymaps.lua

--- config/.config/nvim/lua/plugins/lsp.lua
+++ config/.config/nvim/lua/plugins/lsp.lua
@@ -23,8 +23,8 @@ return {
     lazy = false,
     config = function()
       vim.lsp.enable("lua_ls")
+      vim.lsp.enable("ts_ls")
 
-      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
       vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
       vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
       vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, {})
