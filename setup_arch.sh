484344dce297726a99cdf932601d92b3ba19bfa9
Rafael Hurpia da Rocha <git@rafaelhdr.com.br>
2026-08-10 15:49:57 +0200

    fix: paru should be included in servers

--- setup_arch.sh
+++ setup_arch.sh
@@ -6,6 +6,14 @@ echo "Setting up for Arch Linux..."
 sudo pacman -Syu --noconfirm $(cat packages_common.txt)
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+if ! command -v paru &> /dev/null; then
+    echo "paru not found, installing..."
+    sudo pacman -S --needed --noconfirm base-devel git
+    git clone https://aur.archlinux.org/paru.git /tmp/paru
+    (cd /tmp/paru && makepkg -si --noconfirm)
+    rm -rf /tmp/paru
+fi
+
 if [ "$INCLUDE_HYPRLAND" == "true" ]; then
     sudo pacman -S --noconfirm $(cat packages_desktop.txt)
     ./setup_hyprland.sh
--- setup_hyprland.sh
+++ setup_hyprland.sh
@@ -3,12 +3,4 @@
 echo "Setting up Hyprland..."
 sudo pacman -S --noconfirm $(cat packages_hyprland.txt)
 
-if ! command -v paru &> /dev/null; then
-    echo "paru not found, installing..."
-    sudo pacman -S --needed --noconfirm base-devel git
-    git clone https://aur.archlinux.org/paru.git /tmp/paru
-    (cd /tmp/paru && makepkg -si --noconfirm)
-    rm -rf /tmp/paru
-fi
-
 paru -S --noconfirm $(cat packages_hyprland_aur.txt)
