b81bce3ce7be4cdde5cfd1cd6576f7a5c22fb2b6
Rafael Hurpia da Rocha <git@rafaelhdr.com.br>
2026-08-28 22:37:33 +0200

    feat: dark hyprlock

--- config/.config/hypr/keybindings.lua
+++ config/.config/hypr/keybindings.lua
@@ -8,6 +8,7 @@ local mainMod = "SUPER" -- Sets "Windows" key as main modifier
 
 hl.bind("CTRL + ALT + T", hl.dsp.exec_cmd("GTK_IM_MODULE=simple " .. vars.terminal))
 hl.bind("CTRL + ALT + SHIFT + L", hl.dsp.exec_cmd("hyprlock"))
+hl.bind("CTRL + ALT + SHIFT + SEMICOLON", hl.dsp.exec_cmd("hyprlock -c ~/.config/hypr/hyprlock-dark.conf"))
 hl.bind(mainMod .. " + Z", hl.dsp.exec_cmd("zen-browser -P"))
 hl.bind(mainMod .. " + N", hl.dsp.exec_cmd(vars.fileManager))
 hl.bind(mainMod .. " + S", hl.dsp.exec_cmd("scrcpy --turn-screen-off -e"))
